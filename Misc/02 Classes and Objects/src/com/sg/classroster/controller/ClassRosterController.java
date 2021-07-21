f78034bc2b3a4ba24c5efd85135f66d85e5ea4fa
B-Rust <44529699+B-Rust@users.noreply.github.com>
2021-07-21 10:11:02 -0500

    july 21st commit

---
+++ Misc/02 Classes and Objects/src/com/sg/classroster/controller/ClassRosterController.java
@@ -0,0 +1,57 @@
+package com.sg.classroster.controller;
+
+import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoFileImpl;
+import com.sg.classroster.dto.Student;
+import com.sg.classroster.ui.*;
+
+public class ClassRosterController {
+    private UserIO io = new UserIOConsoleImpl();
+    private ClassRosterView view = new ClassRosterView();
+    private ClassRosterDao dao = new ClassRosterDaoFileImpl();
+    
+    
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            
+            menuSelection = getMenuSelection();
+
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    createStudent();
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        }
+        io.print("GOOD BYE");
+    }
+
+    public int getMenuSelection() {
+    	return view.printMenuAndGetSelection();
+    }
+    
+    private void createStudent() {
+        view.displayCreateStudentBanner();
+        Student newStudent = view.getNewStudentInfo();
+        dao.addStudent(newStudent.getStudentId(), newStudent);
+        view.displayCreateSuccessBanner();
+    }
+    
+}
---
+++ Misc/02 Classes and Objects/src/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -0,0 +1,32 @@
+package com.sg.classroster.dao;
+
+import com.sg.classroster.dto.Student;
+import java.util.*;
+
+public class ClassRosterDaoFileImpl implements ClassRosterDao {
+	
+	private Map<String, Student> students = new HashMap<>();
+	
+    @Override
+    public Student addStudent(String studentId, Student student) {
+        Student prevStudent = students.put(studentId, student);
+        return prevStudent;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Student getStudent(String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Student removeStudent(String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+	
+}
